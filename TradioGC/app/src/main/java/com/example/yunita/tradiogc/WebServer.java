79752dd5aaa8037f6af93b10598444df286ac5bf
ayunita <yunita@ualberta.ca>
2015-11-24 20:52:17 -0700

    webserver moved to /user

--- TradioGC/app/src/main/java/com/example/yunita/tradiogc/WebServer.java
+++ TradioGC/app/src/main/java/com/example/yunita/tradiogc/WebServer.java
@@ -1,9 +1,9 @@
 package com.example.yunita.tradiogc;
 
 public class WebServer {
-    private static final String RESOURCE_URL = "http://cmput301.softwareprocess.es:8080/cmput301f15t09/y/";
+    private static final String RESOURCE_URL = "http://cmput301.softwareprocess.es:8080/cmput301f15t09/user/";
     // specify the limit size of elastic search (its default = 10)
-    private static final String SEARCH_URL = "http://cmput301.softwareprocess.es:8080/cmput301f15t09/y/_search?size=1000000";
+    private static final String SEARCH_URL = "http://cmput301.softwareprocess.es:8080/cmput301f15t09/user/_search?size=1000000";
 
     /**
      * Class constructor.

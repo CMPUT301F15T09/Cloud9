12428db6f503338bf2609192ab9aef6f5b345a57
shuming2 <zsmingwh@gmail.com>
2015-11-29 20:12:59 -0700

    add switch button in profile.xml

--- TradioGC/app/src/main/java/com/example/yunita/tradiogc/profile/ProfileActivity.java
+++ TradioGC/app/src/main/java/com/example/yunita/tradiogc/profile/ProfileActivity.java
@@ -34,7 +34,6 @@ public class ProfileActivity extends AppCompatActivity {
     private ImageButton edit_button;
     private Button myinventory_button;
     private Button friendinventory_button;
-
     private Button addfriend_button;
 
     private TextView username;
